9349ef7b5879ee33916c8b7969f511dda4989aa3
Desi6ner <semaraademia@gmail.com>
2024-01-06 19:49:39 +0000

    share the code

--- front/src/components/Card.jsx
+++ front/src/components/Card.jsx
@@ -9,8 +9,8 @@ function Card({ item, setSelected }) {
     >
       <motion.img
         layoutId={`card-${item.id}`}
-        className="w-full z-20"
-        src={""}
+        className="w-full z-20   "
+        src={item.url}
       />
 
       <div className=" transition-all duration-300 group-hover:bottom-0  group-hover:opacity-100 flex absolute -bottom-32  w-full h-full  bg-black/50  opacity-0 justify-center items-center">
